5a2d174737901fe2e0a18718d1a430929900d3f8
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-08-11 20:50:16 +0300

    HW-05-Without test

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
 
